5ac82c7283bc79bea9db2af133825464ead941ae
unknown <amitmishra.bst@rediffmail.com>
2018-02-23 12:58:29 +0000

    projecthomepage

---
+++ source/client/carltonleisurepro/src/app/components/searchengineflights/searchengineflights.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchengineflights',
+  templateUrl: './searchengineflights.component.html',
+  styleUrls: ['./searchengineflights.component.css']
+})
+export class SearchengineflightsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
