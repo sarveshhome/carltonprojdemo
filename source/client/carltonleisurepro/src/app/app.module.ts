b24aa08ee2537c0875c93ed891b345c5e8317e59
unknown <amitmishra.bst@rediffmail.com>
2018-02-23 11:19:15 +0000

    projectsetup

--- source/client/carltonleisurepro/src/app/app.module.ts
+++ source/client/carltonleisurepro/src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
+
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
+
+const appRoutes: Routes =  [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegisterComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProfileComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ source/client/carltonleisurepro/src/app/services/auth.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
+
+@Injectable()
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private http: Http) {
+       //this.isDev = false;  // Change to false before deployment
+      }
+
+  registerUser(user) {
+    console.log(user);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3060/user/register', user, {headers: headers})
+      .map(res => res.json());
+  }
+
+  authenticateUser(user) {
+    let headers = new Headers();
+    console.log(user);
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3060/user/authenticate', user, {headers: headers})
+      .map(res => res.json());
+  }
+
+  getProfile() {
+    const headersProfile = new Headers();
+    this.loadToken();
+    headersProfile.append('Authorization', this.authToken);
+    headersProfile.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3060/user/profile', {headers: headersProfile})
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired('id_token');
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+}
+}
